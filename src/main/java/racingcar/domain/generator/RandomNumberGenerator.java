5779d8b26448f603b91682f0a508e848383d9056
smini <sm020320@naver.com>
2025-10-24 13:12:17 +0900

    feat(generator): NumberGenerator 인터페이스 구현 및 RandomNumberGenerator 추가

---
+++ src/main/java/racingcar/domain/generator/RandomNumberGenerator.java
@@ -0,0 +1,14 @@
+package racingcar.domain.generator;
+
+import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
+
+public class RandomNumberGenerator implements NumberGenerator {
+
+    private static final int MIN_VALUE = 0;
+    private static final int MAX_VALUE = 9;
+
+    @Override
+    public int getNumber() {
+        return pickNumberInRange(MIN_VALUE, MAX_VALUE);
+    }
+}
