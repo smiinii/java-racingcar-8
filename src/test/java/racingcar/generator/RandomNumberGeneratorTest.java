b4ecfcd094be9d04cc77dbd0f1618cfc2e50b632
smini <sm020320@naver.com>
2025-10-24 13:14:27 +0900

    test(numbergenerator): 난수 생성기 범위 검증 테스트 추가

---
+++ src/test/java/racingcar/generator/RandomNumberGeneratorTest.java
@@ -0,0 +1,24 @@
+package racingcar.generator;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import racingcar.domain.generator.NumberGenerator;
+import racingcar.domain.generator.RandomNumberGenerator;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class RandomNumberGeneratorTest {
+
+    @Test
+    @DisplayName("난수 생성기는 0 이상 9 이하의 수를 반환한다")
+    void generateNumberWithinRange() {
+        // given
+        NumberGenerator numberGenerator = new RandomNumberGenerator();
+        // when & then
+        for (int i = 0; i < 1000; i++) {
+            int number = numberGenerator.getNumber();
+            assertThat(number).isBetween(0, 9);
+        }
+    }
+
+
+}
