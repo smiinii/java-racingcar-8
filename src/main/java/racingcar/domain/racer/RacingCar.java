790a6331f5a8524f6f196dc2c7105addda364b31
smini <sm020320@naver.com>
2025-10-24 20:41:08 +0900

    feat(racingcar): 자동차 이름 길이 제한 기능 추가
    - 하드 코딩 제거 및 상수화

--- src/main/java/racingcar/domain/racer/RacingCar.java
+++ src/main/java/racingcar/domain/racer/RacingCar.java
@@ -2,25 +2,29 @@ package racingcar.domain.racer;
 
 public class RacingCar implements Racer {
 
+    private static final int MOVE_CONDITION = 4;
     private static final int SPEED = 1;
+    private static final int MAX_CARNAME_LENGTH = 5;
     private final String carName;
     private final int distance;
 
     public RacingCar(String carName) {
         validateCarNameIsAlphabetic(carName);
+        validateCarNameLength(carName);
         this.carName = carName;
         this.distance = 0;
     }
 
     public RacingCar(String carName, int distance) {
         validateCarNameIsAlphabetic(carName);
+        validateCarNameLength(carName);
         this.carName = carName;
         this.distance = distance;
     }
 
     @Override
     public Racer move(int randomNumber) {
-        if (randomNumber >= 4) {
+        if (randomNumber >= MOVE_CONDITION) {
             return new RacingCar(carName, distance + SPEED);
         }
         return this;
@@ -37,6 +41,12 @@ public class RacingCar implements Racer {
     private void validateCarNameIsAlphabetic(String carName) {
         if (!carName.matches("^[a-zA-Z]+$")) {
             throw new IllegalArgumentException("자동차 이름은 영어만 허용합니다.");
+        }
+    }
+
+    private void validateCarNameLength(String carName) {
+        if (carName.length() > MAX_CARNAME_LENGTH) {
+            throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.");
         }
     }
 
