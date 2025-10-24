be1b5ce29816a40c3db317a6e6d654c1183c9440
smini <sm020320@naver.com>
2025-10-24 20:25:37 +0900

    test(racingcar): 자동차 이름 영어 검증 테스트 추가

--- src/test/java/racingcar/domain/racer/RacingCarTest.java
+++ src/test/java/racingcar/domain/racer/RacingCarTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class RacingCarTest {
 
@@ -27,5 +28,30 @@ public class RacingCarTest {
         RacingCar stopRacer = (RacingCar)racer.move(3);
         // then
         assertThat(stopRacer.getDistance()).isEqualTo(0);
+    }
+
+    @Test
+    @DisplayName("자동차 이름이 영어가 아닐 시 예외처리")
+    void carNameIsAlphabeticTest() {
+        // given & when & then
+        assertThatThrownBy(() -> new RacingCar("123"))
+                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> new RacingCar("성민"))
+                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> new RacingCar("!!!"))
+                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> new RacingCar("smini1"))
+                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> new RacingCar("smini!"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("자동차 이름이 영어면 통과")
+    void carNameIsAlphabeticTest2() {
+        // given
+        RacingCar racer = new RacingCar("smini");
+        // then
+        assertThat(racer.getCarName()).isEqualTo("smini");
     }
 }
