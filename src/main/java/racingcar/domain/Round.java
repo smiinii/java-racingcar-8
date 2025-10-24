56e2699957e1ca6b62415ed172795b434a423faf
smini <sm020320@naver.com>
2025-10-24 17:24:23 +0900

    feat(round): 값이 비어있으면 예외처리 기능 추가

--- src/main/java/racingcar/domain/Round.java
+++ src/main/java/racingcar/domain/Round.java
@@ -1,4 +1,21 @@
 package racingcar.domain;
 
 public class Round {
+
+    private final int roundNumber;
+
+    public Round(String input) {
+        validateEmpty(input);
+        this.roundNumber = Integer.parseInt(input);
+    }
+
+    public int getRoundNumber() {
+        return roundNumber;
+    }
+
+    private void validateEmpty(String input) {
+        if (input == null || input.isBlank()) {
+            throw new IllegalArgumentException("입력이 비어있습니다.");
+        }
+    }
 }
