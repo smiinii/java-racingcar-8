d03e6249767dc429d5588e07bdd41c3e8e073e80
smini <sm020320@naver.com>
2025-10-24 17:42:04 +0900

    test(round): 숫자 검증 테스트 2개 추가
    - 숫자가 아니면 예외처리
    - 숫자가 맞으면 통과

--- src/test/java/racingcar/RoundTest.java
+++ src/test/java/racingcar/RoundTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import racingcar.domain.Round;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class RoundTest {
@@ -16,5 +17,24 @@ public class RoundTest {
                 .isInstanceOf(IllegalArgumentException.class);
         assertThatThrownBy(() -> new Round("  "))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+    
+    @Test
+    @DisplayName("숫자가 아니면 예외처리")
+    void vaildateDigitTest() {
+        // given & when & then
+        assertThatThrownBy(() -> new Round("a"))
+                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> new Round("^"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("숫자가 맞으면 통과")
+    void trueDigitTest() {
+        // given
+        Round round = new Round("10");
+        // then
+        assertThat(round.getRoundNumber()).isEqualTo(10);
     }
 }
