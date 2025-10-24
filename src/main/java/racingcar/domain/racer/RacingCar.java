20de9bf5a80e5d645c45ad682b52d29371e9504f
smini <sm020320@naver.com>
2025-10-24 19:56:55 +0900

    feat(racingcar): move 기능 추가
    - 무작위 값이 4 이상일 경우 전진한다.

--- src/main/java/racingcar/domain/racer/RacingCar.java
+++ src/main/java/racingcar/domain/racer/RacingCar.java
@@ -1,4 +1,35 @@
 package racingcar.domain.racer;
 
-public class RacingCar {
+public class RacingCar implements Racer {
+
+    private static final int SPEED = 1;
+    private final String carName;
+    private final int distance;
+
+    public RacingCar(String carName) {
+        this.carName = carName;
+        this.distance = 0;
+    }
+
+    public RacingCar(String carName, int distance) {
+        this.carName = carName;
+        this.distance = distance;
+    }
+
+    @Override
+    public Racer move(int randomNumber) {
+        if (randomNumber >= 4) {
+            return new RacingCar(carName, distance + SPEED);
+        }
+        return this;
+    }
+
+    public String getCarName() {
+        return carName;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+
 }
