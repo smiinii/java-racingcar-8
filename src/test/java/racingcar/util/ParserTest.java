ddf3c7490ced94917f7a73457bc835f695c9e38f
smini <sm020320@naver.com>
2025-10-24 14:48:12 +0900

    test(parser): 구분자 분리 검증 테스트 추가

--- src/test/java/racingcar/util/ParserTest.java
+++ src/test/java/racingcar/util/ParserTest.java
@@ -3,13 +3,16 @@ package racingcar.util;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 public class ParserTest {
 
     @Test
     @DisplayName("입력이 비어있으면 예외처리")
-    void inputEmpty() {
+    void inputEmptyTest() {
         // given
         Parser parser = new Parser();
         // when & then
@@ -17,5 +20,16 @@ public class ParserTest {
                 .isInstanceOf(IllegalArgumentException.class);
         assertThatThrownBy(() -> parser.parseCarNames(null))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("구분자로 분리")
+    void inputGapRemoveTest() {
+        // given
+        Parser parser = new Parser();
+        // when
+        List<String> carNames = parser.parseCarNames(" smini, sm");
+        // then
+        assertThat(carNames).containsExactly(" smini", " sm");
     }
 }
