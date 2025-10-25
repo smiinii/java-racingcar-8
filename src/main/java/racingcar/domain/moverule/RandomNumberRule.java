d536991dc087d8e86b474b37c59df8ce2bb055b4
smini <sm020320@naver.com>
2025-10-25 23:44:01 +0900

    feat(randomnumberrule): 무작위 값 기반 이동 규칙(RandomNumberRule) 추가

---
+++ src/main/java/racingcar/domain/moverule/RandomNumberRule.java
@@ -0,0 +1,18 @@
+package racingcar.domain.moverule;
+
+import racingcar.util.generator.NumberGenerator;
+
+public class RandomNumberRule implements MoveRule{
+
+    private static final int MOVE_CONDITION = 4;
+    private final NumberGenerator numberGenerator;
+
+    public RandomNumberRule(NumberGenerator numberGenerator) {
+        this.numberGenerator = numberGenerator;
+    }
+
+    @Override
+    public boolean canMove() {
+        return numberGenerator.getNumber() >= MOVE_CONDITION;
+    }
+}
