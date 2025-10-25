cffc53bc94f187d4878727075740c650b8e13715
smini <sm020320@naver.com>
2025-10-25 18:47:35 +0900

    fear(outputview): 출력 기능 추가
    - 라운드별 경기 결과 출력
    - 최종 우승자 출력

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,4 +1,19 @@
 package racingcar.view;
 
+import racingcar.domain.racer.Racers;
+
+import java.util.Map;
+
 public class OutputView {
+
+    public void printRoundResult(Map<String, Integer> roundResult) {
+        System.out.println("실행 결과");
+        for(int i = 0; i < roundResult.size(); i++) {
+            roundResult.forEach((k, v) -> System.out.println(k + " : " + v));
+        }
+    }
+
+    public void printGameResult(String gameResult) {
+        System.out.println( "최종 우승자 : " + gameResult);
+    }
 }
