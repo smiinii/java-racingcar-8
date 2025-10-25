c60893b25ddf091d98643bc4aff704f464ea63c5
smini <sm020320@naver.com>
2025-10-25 13:56:56 +0900

    test(roundresult): 각 라운드별 모든 자동차 이름 및 이동거리 확인 테스트 추가

---
+++ src/test/java/racingcar/domain/result/RoundResultTest.java
@@ -0,0 +1,50 @@
+package racingcar.domain.result;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import racingcar.domain.Round;
+import racingcar.domain.racer.Racer;
+import racingcar.domain.racer.Racers;
+import racingcar.domain.racer.RacingCar;
+import racingcar.domain.racer.RacingCars;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class RoundResultTest {
+
+    @Test
+    @DisplayName("각 라운드별 모든 자동차 이름 및 이동거리 확인")
+    void roundResultTest() {
+        // given
+        List<Racer> racer = List.of(
+                new RacingCar("smini"),
+                new RacingCar("lsm"),
+                new RacingCar("sm")
+        );
+        Racers racers = new RacingCars(racer);
+        Racers racers1 = new RacingCars(racer);
+        Round round = new Round("3");
+        RoundResult roundResult = new RoundResult();
+        // when & then
+        for (int i = 1; i < round.getRoundNumber() + 1; i++) {
+            racers = racers.moveAll(5);
+            Map<String, Integer> result = roundResult.addRoundResult(racers);
+            assertThat(result).hasSize(3)
+                    .containsEntry("smini", i)
+                    .containsEntry("lsm", i)
+                    .containsEntry("sm", i);
+        }
+        // when & then
+        for (int i = 1; i < round.getRoundNumber() + 1; i++) {
+            racers1 = racers1.moveAll(3);
+            Map<String, Integer> result = roundResult.addRoundResult(racers1);
+            assertThat(result).hasSize(3)
+                    .containsEntry("smini", 0)
+                    .containsEntry("lsm", 0)
+                    .containsEntry("sm", 0);
+        }
+    }
+}
