fcc6be153c88272aa1d056577efa279281c56ad4
smini <sm020320@naver.com>
2025-10-25 13:56:16 +0900

    feat(roundresult): 각 라운드 마다 모든 자동차의 이름과 이동거리를 구하는 기능 추가

--- src/main/java/racingcar/domain/result/RoundResult.java
+++ src/main/java/racingcar/domain/result/RoundResult.java
@@ -1,4 +1,18 @@
 package racingcar.domain.result;
 
+import racingcar.domain.racer.Racers;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class RoundResult {
+
+    public Map<String, Integer> addRoundResult(Racers racers) {
+        Map<String, Integer> result = new LinkedHashMap<>();
+        for (int i = 0; i < racers.getRacers().size(); i++) {
+            result.put(racers.getRacers().get(i).getName(),
+                    racers.getRacers().get(i).getDistance());
+        }
+        return result;
+    }
 }
